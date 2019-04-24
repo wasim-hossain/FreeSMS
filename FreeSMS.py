924682b2d1802a42a4637f3897d7b3e735a6e54e
wasimind <49933132+wasimind@users.noreply.github.com>
2019-04-24 09:59:18 +0530

    Update FreeSMS.py

--- FreeSMS.py
+++ FreeSMS.py
@@ -4,9 +4,9 @@ import requests
 
 url = "https://www.fast2sms.com/dev/bulk"
 
-API_KEY = 'ewzjER9x1hbY3uvLmkZPAHVFy2NO7aMTWCi6lXUp80GntQoD4ccaY7PICAO2k8oW5v3UqsQRBpmV0J9G'
-msg = "Successfully sent SMS. Do not share it with others. N.B-This is a test message"
-numbers = "8972666690,903866668"
+API_KEY = 'ewzjER9x1hbY3uvLmkZPAHVFy2NO7aMTWCi6rrtfghhjGntQoD4ccaY7PICAO2k8oW5v3UqsQRBpmV0J9G'  #Use your API Key
+msg = "Successfully sent SMS. Do not share it with others. N.B-This is a test message"  #Type your message
+numbers = "8972666690,903866668" #List all the numbers where you want to send SMS
 querystring = {"authorization":API_KEY,"sender_id":"FSTSMS","message": msg,"language":"english","route":"p","numbers":numbers}
 
 headers = {
